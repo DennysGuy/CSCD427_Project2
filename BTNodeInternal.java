1d68ef0dc7cc60bd4fc41bd621c43a04f728865f
Matt Matriciano <mmatriciano@ewu.edu>
2024-04-25 11:06:19 -0700

    added project files

---
+++ BTNodeInternal.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class BTNodeInternal extends BTNode
+{
+   public ArrayList<BTNode> children;
+   
+   public BTNodeInternal() 
+   {
+
+   }
+   
+   public void insert(String key, BPlusTree tree)
+   {
+      
+   }
+   
+   public void printLeavesInSequence()
+   {
+      
+   }
+   
+   public void printStructureWKeys()
+   {
+      
+   }
+
+   public int findInsertIndex(String word) {
+      return 0;
+   }
+
+   public Boolean rangeSearch(String startWord, String endWord)
+   {
+      return true;
+   }
+   
+   public Boolean searchWord(String word)
+   {
+      return true;
+   }
+}
---
+++ BTNodeLeaf.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class BTNodeLeaf extends BTNode
+{
+   public ArrayList<Integer> keyCounts;
+   public BTNodeLeaf nextLeaf;
+   
+   public BTNodeLeaf()
+   {
+      
+   }
+   
+   public void insert(String word, BPlusTree tree)
+   {
+
+   }
+   
+   public void printLeavesInSequence()
+   {
+      
+   }
+   
+   public void printStructureWKeys()
+   {
+      
+   }
+
+   public int findInsertIndex(String word) {
+      return 0;
+   }
+
+   public Boolean rangeSearch(String startWord, String endWord)
+   {
+      return true;
+   }
+   
+   public Boolean searchWord(String word){
+      return false;
+   }
+   {
+   }
+}
